0caf8fd5aa93c83b2422b264e6973ef6633021c9
Math0424 <Calculus0424@gmail.com>
2023-09-30 15:04:02 -0500

    Initial structs for backend

---
+++ HackathonBackend/src/Structs.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackathonBackend.src
+{
+    internal class Structs
+    {
+        enum NoteCategory
+        {
+            Defects,
+            History,
+            Information,
+            Behavior,
+        }
+
+        struct User
+        {
+            ulong id;
+            string username;
+            string encriptedPassword;
+            string salt;
+            DateTime lastLogin;
+        }
+
+        struct Bovine
+        {
+            ulong id;
+            ulong ownerId;
+
+            string name;
+            bool male;
+
+            ulong father;
+            ulong mother;
+
+            DateTime birth;
+            DateTime death;
+
+            bool cull;
+            bool culled;
+            bool casterated;
+        }
+
+        struct BreedingHistory
+        {
+            ulong bovineId;
+
+            DateTime inseminationDate;
+            DateTime birthedDate;
+            float birthWeight;
+            bool stillborn;
+
+            ulong cow;
+            ulong bull;
+        }
+
+        struct BovineNotes
+        {
+            ulong bovineId;
+
+            int category;
+            DateTime creation;
+            string title;
+            string message;
+        }
+
+    }
+}
