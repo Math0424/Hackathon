c29ee1b983923d818fd00153a041aaa5ba942687
Brandon Zaragoza <brz0323@gmail.com>
2023-09-30 16:09:27 -0500

    added react router

--- Frontend/HackathonFrontend/hackathon/src/index.js
+++ Frontend/HackathonFrontend/hackathon/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import Login from './views/login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Login />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Login />
+        <Routes />
+    </React.StrictMode>
 );
