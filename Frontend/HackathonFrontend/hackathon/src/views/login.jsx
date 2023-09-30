63047587438e092ada735b1e47f763d9c2f7971d
Brandon Zaragoza <brz0323@gmail.com>
2023-09-30 15:44:56 -0500

    build barebones login

---
+++ Frontend/HackathonFrontend/hackathon/src/views/login.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const loginValidates = (username, password) => {
+
+}
+
+const Login = () => {
+    const [username, setusername] = useState("");
+    const [password, setpassword] = useState("");
+    const handleSubmit = (e) => {
+        if(loginValidates(username, password)) {
+
+        }
+    };
+    return (
+        <div>
+            <h1>Please log in</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="Username"
+                    value={username}
+                    onChange={(e) => setusername(e.target.value)}
+                />
+                <input
+                    type="password"
+                    name="Password"
+                    onChange={(e) => setpassword(e.target.value)}
+                />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    )
+}
+
+export default Login;
