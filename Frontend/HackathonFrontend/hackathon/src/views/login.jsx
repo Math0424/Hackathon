717c925b3830225845d891e6d925931d376994af
Brandon Zaragoza <brz0323@gmail.com>
2023-09-30 21:15:40 -0500

    actually validate user credentials

--- Frontend/HackathonFrontend/hackathon/src/views/login.jsx
+++ Frontend/HackathonFrontend/hackathon/src/views/login.jsx
@@ -3,25 +3,22 @@ import {useNavigate} from 'react-router-dom';
 import Cookies from 'js-cookie';
 
 const loginValidates = async (user, pass) => {
-    const response = await fetch('http://localhost:5000/user/auth', {
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, *cors, same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit
+    const response = await fetch('http://173.215.25.174:5000/user/auth', {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
         headers: {
             "Content-Type": "application/json",
-            // 'Content-Type': 'application/x-www-form-urlencoded',
         },
-        redirect: "follow", // manual, *follow, error
-        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        redirect: "follow",
+        referrerPolicy: "no-referrer",
         body: JSON.stringify({
             username: user,
             password: pass
-        }), // body data type must match "Content-Type" header
+        })
     });
 
-    console.log(response.json());
-    return response.json(); // parses JSON response into native JavaScript objects
+    return response.json();
 }
 
 const Login = () => {
@@ -30,8 +27,9 @@ const Login = () => {
     const navigate = useNavigate();
     const handleSubmit = async () => {
         const response = await loginValidates(username, password)
-        if (response.id !== undefined) {
-            Cookies.set('userId', response.id)
+
+        if (response[0] !== undefined) {
+            Cookies.set('userId', response[0])
 
             navigate('/Cow');
         }
@@ -40,21 +38,21 @@ const Login = () => {
     return (
         <div>
             <h1>Please log in</h1>
-                <input
-                    onChange={(e) => setusername(e.target.value)}
-                    placeholder={'Username'}
-                    name="Username"
-                    type="text"
-                    value={username}
-                />
-                <input
-                    name="Password"
-                    onChange={(e) => setpassword(e.target.value)}
-                    placeholder={'Password'}
-                    type="password"
-                />
-                <input type="submit" onClick={handleSubmit} value="Submit" />
-                <input type="button" value="Create Account" />
+            <input
+                onChange={(e) => setusername(e.target.value)}
+                placeholder={'Username'}
+                name="Username"
+                type="text"
+                value={username}
+            />
+            <input
+                name="Password"
+                onChange={(e) => setpassword(e.target.value)}
+                placeholder={'Password'}
+                type="password"
+            />
+            <input type="submit" onClick={handleSubmit} value="Submit" />
+            <input type="button" value="Create Account" />
         </div>
     )
 }
